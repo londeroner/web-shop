d6d9d26eb2540b4bded0e9de1f76cc7929e1e4c8
londeroner <Filin_s1999@mail.ru>
2022-05-13 16:40:15 +0300

    Angular app hierarchy

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { IPagination } from './shared/models/pagination';
+import { IProduct } from './shared/models/product';
 
 @Component({
   selector: 'app-root',
@@ -8,12 +10,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'Web-Shop';
-  products: any[];
+  products: IProduct[];
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe((response: any) => 
+    this.http.get('https://localhost:5001/api/products?pageSize=50')
+    .subscribe((response: IPagination) => 
       {
         this.products = response.data;
       }, error => {
